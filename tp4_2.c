feccc6b6b06ca17abb17b1f85bee93432f5ea759
Tomás Cassas <tomascassas@gmail.com>
2023-05-01 01:19:58 -0300

    2.4 Creacion e implementacion de la mecanica de tareas realizadas

--- tp4_2.c
+++ tp4_2.c
@@ -10,8 +10,10 @@ typedef struct Tarea {
 } Tarea;
 
 // Funciones
+void inicializaTareas(Tarea ** matrizDeTareas, int cantidadDeTareas);
 void cargaTareas(Tarea ** matrizDeTareas, int cantidadDeTareas);
 void mostrarTareas(Tarea ** matrizDeTareas, int cantidadDeTareas);
+void administrarEstadoTareas(Tarea ** matrizDeTareas, int cantidadDeTareas, Tarea ** matrizDeTareasRealizadas);
 
 int main() {
   // Seed rand()
@@ -22,25 +24,34 @@ int main() {
   scanf("%d%*c", &cantidadDeTareas);
 
   Tarea ** matrizDeTareas = (Tarea **) malloc(cantidadDeTareas * sizeof(Tarea **));
+  inicializaTareas(matrizDeTareas, cantidadDeTareas);
 
   cargaTareas(matrizDeTareas, cantidadDeTareas);
   mostrarTareas(matrizDeTareas, cantidadDeTareas);
 
-  // Tarea ** tareasRealizadas = (Tarea **) malloc(cantidadDeTareas * sizeof(Tarea **));
-  // Tarea ** tareasNoRealizadas = (Tarea **) malloc(cantidadDeTareas * sizeof(Tarea **));
+  Tarea ** tareasRealizadas = (Tarea **) malloc(cantidadDeTareas * sizeof(Tarea **));
+  inicializaTareas(tareasRealizadas, cantidadDeTareas);
+
+  administrarEstadoTareas(matrizDeTareas, cantidadDeTareas, tareasRealizadas);
+
+  printf("-Tareas luego de reorganizar:\n");
+  mostrarTareas(tareasRealizadas, cantidadDeTareas);
 
   return 0;
 }
 
-void cargaTareas(Tarea ** matrizDeTareas, int cantidadDeTareas) {
-  // Inicializa arrays en NULL
+void inicializaTareas(Tarea ** matrizDeTareas, int cantidadDeTareas) {
   int i;
 
   for(i = 0; i < cantidadDeTareas; i++) {
     matrizDeTareas[i] = NULL;
   }
+}
 
+void cargaTareas(Tarea ** matrizDeTareas, int cantidadDeTareas) {
   // Carga tareas
+  int i;
+
   for(i = 0; i < cantidadDeTareas; i++) {
     char * buffer = (char *) malloc(100 * sizeof(char *));
     matrizDeTareas[i] = (Tarea *) malloc(sizeof(Tarea *));
@@ -72,8 +83,43 @@ void mostrarTareas(Tarea ** matrizDeTareas, int cantidadDeTareas) {
   int i;
 
   for(i = 0; i < cantidadDeTareas; i++) {
-    printf("-Tarea N° %d\n", matrizDeTareas[i]->TareaID);
-    printf("\tDescripcion: %s\n", matrizDeTareas[i]->Descripcion);
-    printf("\tDuracion: %d\n\n", matrizDeTareas[i]->Duracion);
+    if(matrizDeTareas[i] != NULL) {
+      printf("\t-Tarea N° %d\n", matrizDeTareas[i]->TareaID);
+      printf("\tDescripcion: %s\n", matrizDeTareas[i]->Descripcion);
+      printf("\tDuracion: %d\n\n", matrizDeTareas[i]->Duracion);
+    }
+  }
+
+  printf("\n");
+}
+
+void administrarEstadoTareas(Tarea ** matrizDeTareas, int cantidadDeTareas, Tarea ** matrizDeTareasRealizadas) {
+  int i;
+  int statusTareaAuxiliar;
+
+  printf("A continuación se listarán todas las tareas y deberás indicar cuales fueron realizadas\n");
+  for(i = 0; i < cantidadDeTareas; i++) {
+    printf("-Tarea N° %d (%s) ", matrizDeTareas[i]->TareaID,  matrizDeTareas[i]->Descripcion);
+
+    do {
+      printf("¿Esta tarea fue realizada? (1: Si, 2: No): ");
+      scanf("%d%*c", &statusTareaAuxiliar);
+
+      if(statusTareaAuxiliar != 1 && statusTareaAuxiliar != 2) {
+        printf("x Por favor elija una de las opciones mencionadas. ");
+      }
+      printf("\n");
+    } while (statusTareaAuxiliar != 1 && statusTareaAuxiliar != 2);
+
+    if(statusTareaAuxiliar == 1) {
+      matrizDeTareasRealizadas[i] = (Tarea *) malloc(sizeof(Tarea *));
+      matrizDeTareasRealizadas[i]->TareaID = matrizDeTareas[i]->TareaID;
+      matrizDeTareasRealizadas[i]->Duracion = matrizDeTareas[i]->Duracion;
+      matrizDeTareasRealizadas[i]->Descripcion = (char *) malloc(100 * sizeof(char *));
+      strcpy(matrizDeTareasRealizadas[i]->Descripcion, matrizDeTareas[i]->Descripcion);
+      
+      free(matrizDeTareas[i]->Descripcion);
+      matrizDeTareas[i] = NULL;
+    }
   }
 }
